0393e32c5377a455b9a2c0036ac941af46d70fbb
pldomas <pledominykas@gmail.com>
2019-12-23 13:27:26 +0200

    Set up basic server.

---
+++ app.js
@@ -0,0 +1,19 @@
+// Node Modules
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Start server
+app.listen(PORT, function(){
+  console.log(`Server running on port: ${PORT}`);
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
